0a2f97137b7dab0c867241705d80c94862e1f36f
LilLeif <emil.blanksvard@outlook.com>
2025-02-06 21:31:08 +0100

    some updates

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ from pynput import keyboard
 
 class KeyListener:
     def __init__(self):
-        self.keys = ["W", "A", "S", "D"]
-        self.key_vector = [0, 0, 0, 0]
+        self.keys = ["W", "A", "S", "D", "R"]
+        self.key_vector = [0, 0, 0, 0, 0]
         self.listener = keyboard.Listener(on_press=self.press, on_release=self.release)
         self.listener.start()
 
     def press(self, key):
-        if hasattr(key, 'char') and key.char in self.keys:
-            self.key_vector[self.keys.index(key.char)] = 1
+        if hasattr(key, 'char') and key.char.upper() in self.keys:
+            self.key_vector[self.keys.index(key.char.upper())] = 1
 
     def release(self, key):
-        if hasattr(key, 'char') and key.char in self.keys:
-            self.key_vector[self.keys.index(key.char)] = 0
+        if hasattr(key, 'char') and key.char.upper() in self.keys:
+            self.key_vector[self.keys.index(key.char.upper())] = 0
 
     def get_keys(self):
         return self.key_vector
+
+    def reset_keys(self):
+        self.key_vector = [0, 0, 0, 0, 0]
 
 
 key_listener = KeyListener()
@@ -42,13 +45,22 @@ def running():
 
 hotkey_listener = keyboard.GlobalHotKeys({'<ctrl>+<shift>+o': running})
 hotkey_listener.start()
+
 run = False
 
-
-while run:
-    img = capture_img()
-    keys = key_listener.get_keys()
-
-    time.sleep(0.1)
-
-
+images = []
+key_states = []
+while True:
+    if run:
+        img = capture_img()
+        keys = key_listener.get_keys()
+        images.append(img)
+        key_states.append(keys)
+        if keys[4] == 1:
+            name = f"episodes/ep_{str(time.time())}"
+            np.savez(name + ".npz",
+                     frames=np.array(images, dtype=np.uint8),
+                     keys=np.array(key_states, dtype=np.uint8))
+            print(name + " saved!")
+            key_listener.reset_keys()
+    time.sleep(0.01)
